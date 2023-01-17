34d0eb79e7073efd9304dc2ce49a55ae9841fafd
svkropi@gmail.com <svkropi@gmail.com>
2023-01-17 19:32:11 +0100

    v.0.02 / added bitpanda table - not completed

--- index.php
+++ index.php
@@ -10,8 +10,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql_query = "SELECT Coin, Fees FROM kraken";
-$result = mysqli_query($conn, $sql_query);
+$sql_query_kraken = "SELECT Coin, Fees FROM kraken";
+$result_kraken = mysqli_query($conn, $sql_query_kraken);
+
+$sql_query_bitpanda = "SELECT Coin, Fee FROM bitpanda";
+$result_bitpanda = mysqli_query($conn, $sql_query_bitpanda);
 ?>
 
 <!DOCTYPE html>
@@ -21,24 +24,49 @@ $result = mysqli_query($conn, $sql_query);
     <title>
         WithdrawalCompare
     </title>
+    <style><?php include 'style.css'; ?></style>
 </head>
 
 <body>
     <h2>
-        Kraken
+        Kraken / Bitpanda
     </h2>
-    <p>
-        <?php
-        if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo $r = $row["Coin"] . " | " . $row["Fees"] . "<br>";
-            }
-        } else {
-            echo "0 results";
-        }
-        ?>
-    </p>
+    <table style="float: left">
+        <thead>
+            <tr>
+                <th>Coin</th>
+                <th>Fees</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if(mysqli_num_rows($result_kraken) > 0){?>
+                <?php while($row = mysqli_fetch_assoc($result_kraken)) { ?>
+                    <tr>
+                        <td><?php echo $row["Coin"] ?></td>
+                        <td><?php echo $row["Fees"] ?></td>
+                    </tr>
+                <?php } ?>
+            <?php } ?>
+        </tbody>
+    </table>
+    <table style="float: left">
+        <thead>
+            <tr>
+                <th>Coin</th>
+                <th>Fees</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if(mysqli_num_rows($result_bitpanda) > 0){?>
+                <?php while($row = mysqli_fetch_assoc($result_bitpanda)) { ?>
+                    <tr>
+                        <td><?php echo $row["Coin"] ?></td>
+                        <td><?php echo $row["Fee"] ?></td>
+                    </tr>
+                <?php } ?>
+            <?php } ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
